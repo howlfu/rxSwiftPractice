0c8903ffc619619fdf38ecde17a3404a7d74128a
howlfu <howlfu@gmail.com>
2022-09-12 12:09:07 +0800

    add main ui

---
+++ rxSwiftPractice/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  rxSwiftPractice
+//
+//  Created by Howlfu on 2022/9/12.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var usernameOutlet: UITextField!
+    @IBOutlet weak var usernameValidOutlet: UILabel!
+    
+    @IBOutlet weak var passwordOutlet: UITextField!
+    @IBOutlet weak var passwordValidOutlet: UILabel!
+    @IBOutlet weak var loginBtnOutlet: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
