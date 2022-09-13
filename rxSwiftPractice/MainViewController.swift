09d4ae800ee39f017b6a354eb3372e0c34069ebd
howlfu <howlfu@gmail.com>
2022-09-13 11:14:37 +0800

    add main view binding

--- rxSwiftPractice/MainViewController.swift
+++ rxSwiftPractice/MainViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class MainViewController: UIViewController {
 
@@ -15,12 +17,44 @@ class MainViewController: UIViewController {
     @IBOutlet weak var passwordOutlet: UITextField!
     @IBOutlet weak var passwordValidOutlet: UILabel!
     @IBOutlet weak var loginBtnOutlet: UIButton!
-    
+    var disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        usernameValidOutlet.text = "Username at least 5 characters"
+        passwordValidOutlet.text = "Password at least 5 characters"
+        bindingAll()
+    }
+    
+    private func bindingAll() {
+        let userNameOver5 = getObservableBool(target: usernameOutlet)
+        let userNameEmpty = usernameOutlet.rx.text.orEmpty.map{
+                    $0.count == 0 } .share(replay: 1)
+        let userNameValid = Observable.combineLatest(userNameEmpty, userNameOver5) {$0 || $1}.share(replay: 1)
+        self.bindingElement(obser: userNameValid, binder: self.usernameValidOutlet.rx.isHidden)
+        self.bindingElement(obser: userNameOver5, binder: self.passwordOutlet.rx.isEnabled)
+        
+        let passwordOver5 = getObservableBool(target: passwordOutlet)
+        let passwordEmpty = passwordOutlet.rx.text.orEmpty.map{
+                    $0.count == 0 } .share(replay: 1)
+        let passwordValid = Observable.combineLatest(passwordEmpty, passwordOver5) {$0 || $1}.share(replay: 1)
+        self.bindingElement(obser: passwordValid, binder: self.passwordValidOutlet.rx.isHidden)
+        
+        let everyThingValid = Observable.combineLatest(userNameOver5, passwordOver5) {$0 && $1}.share(replay: 1)
+        self.bindingElement(obser: everyThingValid, binder: self.loginBtnOutlet.rx.isEnabled)
+        
+    }
+    
+    private func getObservableBool(target: UITextField) -> Observable<Bool> {
+        return target.rx.text.orEmpty.map{
+            $0.count >= 5
+        } .share(replay: 1)
     }
 
+    private func bindingElement(obser: Observable<Bool>, binder: Binder<Bool>) {
+        obser.bind(to:binder).disposed(by: disposeBag)
+    }
+    
 
 }
 
